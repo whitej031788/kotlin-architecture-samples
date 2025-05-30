bfecf05f7fadacbe2b4e14e63301db198ce4cffc
Jamie White <jamie.white@sonarsource.com>
2025-05-30 10:16:31 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,9 @@ sonar {
 allprojects {
     configurations.all {
         resolutionStrategy {
-            // Force a specific version to prevent conflicts
-            force("org.bouncycastle:bcprov-jdk15to18:1.70")
-            force("org.bouncycastle:bcpkix-jdk15to18:1.70")
+            force("org.bouncycastle:bcprov-jdk15to18:1.79") # Or 1.80, or 1.77 if 1.79/1.80 cause new issues
+            force("org.bouncycastle:bcpkix-jdk15to18:1.79") # Match version
+            force("org.bouncycastle:bcutil-jdk15to18:1.79") # Crucial: bcutil contains EdECObjectIdentifiers in newer versions
         }
     }
 }
